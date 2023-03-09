d20b1f50d65ad401095527ae5a3438dd50b719d7
MaksymBratsiun <bratsyun_m@ukr.net>
2023-03-09 12:37:04 +0200

    add crud, create db, fix models

--- connect.py
+++ connect.py
@@ -1,15 +1,14 @@
 import configparser
 
-from mongoengine import connect
+from mongoengine import *
 
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-mongo_user = config.get('DB', 'user')
-mongodb_pass = config.get('DB', 'pass')
-db_name = config.get('DB', 'db_name')
-domain = config.get('DB', 'domain')
+mongo_user = config.get('DB_DEV', 'user')
+mongodb_pass = config.get('DB_DEV', 'password')
+db_name = config.get('DB_DEV', 'db_name')
+domain = config.get('DB_DEV', 'domain')
 
 connect(host=f'mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority')
-
---
+++ models.py
@@ -0,0 +1,15 @@
+from mongoengine import Document
+from mongoengine.fields import ListField, StringField, ReferenceField
+
+
+class Author(Document):
+    fullname = StringField(max_length=120, required=True)
+    born_date = StringField(max_length=30)
+    born_location = StringField()
+    description = StringField()
+
+
+class Quote(Document):
+    tags = ListField(StringField(max_length=30))
+    author = ReferenceField(Author, reverse_delete_rule='CASCADE')
+    quote = StringField()
