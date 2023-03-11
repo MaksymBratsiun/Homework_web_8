5adb85e0e42ebf0a30f48f6c1204d13ded1a4e9f
MaksymBratsiun <bratsyun_m@ukr.net>
2023-03-11 16:26:15 +0200

    finish part_2

--- part_2/produser.py
+++ part_2/produser.py
@@ -1,4 +1,5 @@
 from random import choice
+import json
 
 import pika
 from faker import Faker
@@ -12,8 +13,11 @@ NUMBER_USERS = 10
 faker = Faker()
 
 credentials = pika.PlainCredentials('guest', 'guest')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672, credentials=credentials))
+channel = connection.channel()
+channel.exchange_declare(exchange='my_task', exchange_type='direct')
+channel.queue_declare('sending', durable=True)
+channel.queue_bind(exchange='my_task', queue='sending')
 
 
 def create_users(number=NUMBER_USERS):
@@ -34,7 +38,16 @@ def create_users(number=NUMBER_USERS):
 
 
 if __name__ == '__main__':
+
     if not User.objects():
         create_users()
+
     for i in User.objects():
-        print(i.to_mongo().to_dict())
+        body = json.dumps({'id': str(i.id)}).encode()
+        channel.basic_publish(exchange='my_task',
+                              routing_key='sending',
+                              body=body,
+                              properties=pika.BasicProperties(
+                                  delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE))
+        print('Sent %r' % body)
+    connection.close()
